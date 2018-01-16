1aa2fb8a7bb679650fed54c18aa3f116fa57be45
LeungEr <604082432@qq.com>
2018-01-16 18:31:52 +0800

    会员注册/会员中心页面搭建

--- app/home/views.py
+++ app/home/views.py
@@ -24,3 +24,29 @@ def logout():
 @home.route("/register/")
 def register():
     return render_template("home/register.html")
+
+
+@home.route("/user/")
+def user():
+    return render_template("home/user.html")
+
+
+@home.route("/pwd/")
+def pwd():
+    return render_template("home/pwd.html")
+
+
+@home.route("/comments/")
+def comments():
+    return render_template("home/comments.html")
+
+
+@home.route("/loginlog/")
+def loginlog():
+    return render_template("home/loginlog.html")
+
+
+@home.route("/moviecol/")
+def moviecol():
+    return render_template("home/moviecol.html")
+
