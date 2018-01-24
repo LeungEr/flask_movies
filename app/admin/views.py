ac6d4b55d0a658f04571ebe5a476e5e506e9212d
LeungEr <604082432@qq.com>
2018-01-24 12:20:48 +0800

    会员管理(查看，删除)

--- app/admin/views.py
+++ app/admin/views.py
@@ -18,6 +18,7 @@ from app.models import (
     Tag,
     Movie,
     Preview,
+    User,
 )
 from functools import wraps
 from app import (
@@ -317,7 +318,7 @@ def preview_edit(id=None):
         db.session.commit()
         flash("修改预告成功!", "ok")
         return redirect(url_for('admin.preview_edit', id=id))
-    return render_template("admin/preview_edit.html", form=form,preview=preview)
+    return render_template("admin/preview_edit.html", form=form, preview=preview)
 
 
 # 删除预告
@@ -331,16 +332,35 @@ def preview_del(id=None):
     return redirect(url_for('admin.preview_list', page=1))
 
 
-@admin.route("/user/list")
+# 用户列表
+@admin.route("/user/list/<int:page>", methods=["GET"])
 @admin_login_req
-def user_list():
-    return render_template("admin/user_list.html")
+def user_list(page=None):
+    if page is None:
+        page = 1
+    page_data = User.query.order_by(
+        User.addtime.desc()
+    ).paginate(page=page, per_page=10)
+    return render_template("admin/user_list.html", page_data=page_data)
 
 
-@admin.route("/user/view")
+# 查看用户
+@admin.route("/user/view/<int:id>/", methods=["GET"])
 @admin_login_req
-def user_view():
-    return render_template("admin/user_view.html")
+def user_view(id=None):
+    user = User.query.get_or_404(int(id))
+    return render_template("admin/user_view.html", user=user)
+
+
+# 删除用户
+@admin.route("/user/del/<int:id>/", methods=["GET"])
+@admin_login_req
+def user_del(id=None):
+    user = User.query.get_or_404(int(id))
+    db.session.delete(user)
+    db.session.commit()
+    flash("删除用户成功!", "ok")
+    return redirect(url_for('admin.user_list', page=1))
 
 
 @admin.route("/comment/list")
