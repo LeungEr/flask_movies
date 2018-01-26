e38eec206fb52c81bb487290cbc8b1e1d21cadf7
LeungEr <604082432@qq.com>
2018-01-26 18:20:55 +0800

    会员登录日志

--- app/home/views.py
+++ app/home/views.py
@@ -164,10 +164,18 @@ def comments():
     return render_template("home/comments.html")
 
 
-@home.route("/loginlog/")
+# 会员登录日志
+@home.route("/loginlog/<int:page>/", methods=["GET", "POST"])
 @user_login_req
-def loginlog():
-    return render_template("home/loginlog.html")
+def loginlog(page=None):
+    if page is None:
+        page = 1
+    page_data = Userlog.query.filter_by(
+        user_id = int(session["user_id"])
+    ).order_by(
+        Userlog.addtime.desc()
+    ).paginate(page=page, per_page=10)
+    return render_template("home/loginlog.html", page_data=page_data)
 
 
 @home.route("/moviecol/")
